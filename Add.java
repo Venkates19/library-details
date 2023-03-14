95fab90c98ec9346a3bd3c8c0f6e3c8a72e113b3
Venkates19 <100529191+Venkates19@users.noreply.github.com>
2023-03-14 12:54:16 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Add {
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter a value");
+        a=s.nextInt();
+        System.out.println("enter b value");
+        b=s.nextInt();
+        System.out.println("result");
+        System.out.println(a+b);
+    }
+}
